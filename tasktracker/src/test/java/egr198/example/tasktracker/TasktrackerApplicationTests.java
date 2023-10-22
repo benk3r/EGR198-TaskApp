728f2cc24f5c6d0522547bbdaeda9dce1ef1a512
BeccaHurtado <rebeccah7.rh@gmail.com>
2023-10-21 18:08:34 -0700

    made maven project

---
+++ tasktracker/src/test/java/egr198/example/tasktracker/TasktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package egr198.example.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
