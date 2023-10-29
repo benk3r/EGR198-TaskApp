c79211f4f1c92bcf720375f17a36014e3b02e143
BeccaHurtado <rebeccah7.rh@gmail.com>
2023-10-28 20:46:35 -0700

    set up sample data for folders and tasks collections

--- tasktracker/playground-1.mongodb.js
+++ tasktracker/playground-1.mongodb.js
@@ -10,42 +10,67 @@
 // https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
 
 // Select the database to use.
-use('sample_airbnb');
+use('tasktracker_db');
+const collection = 'tasks'
 
-// Insert a few documents into the sales collection.
-//db.getCollection('listingsAndReviews').insertMany
-// ([
-//   { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
-//   { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
-//   { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
-//   { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
-//   { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
-//   { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
-//   { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
-//   { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
+db.tasks.find({})
+
+// db.tasks.insertMany([
+//   {"_id" : 1, 
+//   "task" : "Creating a Database",
+//   "description" : "create a sample database with tasks and folders",
+//   "due" : new Date("2014-03-01T08:00:00Z"),
+//   "priority" : "High"},
+//   {"_id" : 2, 
+//   "task" :  "Reports",
+//   "description" : "Finish up reports for September and October",
+//   "due" : new Date('2014-04-04T11:21:39.736Z'),
+//   "priority" : "Medium"},
+//   {"_id" : 3, 
+//   "task" : "Gym", 
+//   "description" : "Go to gym by end of week",
+//   "due" : new Date('2014-03-01T09:00:00Z'),
+//   "priority" : "Low"}
+// ])
+
+//db.tasks.deleteMany( {} )
+
+
+
+// // Insert a few documents into the sales collection.
+// //db.getCollection('listingsAndReviews').insertMany
+// // ([
+// //   { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
+// //   { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
+// //   { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
+// //   { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
+// //   { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
+// //   { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
+// //   { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
+// //   { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
+// // ]);
+
+// // Run a find command to view items sold on April 4th, 2014.
+// const salesOnApril4th = db.getCollection('listingsAndReviews').find();
+
+// const allSales = db.getCollection('sales').find();
+
+// // Print a message to the output window.
+// console.log(`${salesOnApril4th} sales occurred in 2014.`);
+// //console.log(`${allSales} all sales.`);
+
+// // Here we run an aggregation and open a cursor to the results.
+// // Use '.toArray()' to exhaust the cursor to return the whole result set.
+// // You can use '.hasNext()/.next()' to iterate through the cursor page by page.
+// db.getCollection('sales').aggregate([
+//   // Find all of the sales that occurred in 2014.
+//   { $match: { date: { $gte: new Date('2014-01-01'), 
+//   $lt: new Date('2015-01-01') } 
+//   } },
+
+//   // Group the total sales for each product.
+//   { $group: { 
+//     _id: '$item', 
+//     totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } 
+//   } }
 // ]);
-
-// Run a find command to view items sold on April 4th, 2014.
-const salesOnApril4th = db.getCollection('listingsAndReviews').find();
-
-const allSales = db.getCollection('sales').find();
-
-// Print a message to the output window.
-console.log(`${salesOnApril4th} sales occurred in 2014.`);
-//console.log(`${allSales} all sales.`);
-
-// Here we run an aggregation and open a cursor to the results.
-// Use '.toArray()' to exhaust the cursor to return the whole result set.
-// You can use '.hasNext()/.next()' to iterate through the cursor page by page.
-db.getCollection('sales').aggregate([
-  // Find all of the sales that occurred in 2014.
-  { $match: { date: { $gte: new Date('2014-01-01'), 
-  $lt: new Date('2015-01-01') } 
-  } },
-
-  // Group the total sales for each product.
-  { $group: { 
-    _id: '$item', 
-    totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } 
-  } }
-]);
